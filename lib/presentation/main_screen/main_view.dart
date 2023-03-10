c035de8aa32cf9bea1d07f547f7ff511d607a143
AhmedAbdulla1 <97388228+AhmedAbdulla1@users.noreply.github.com>
2023-03-10 23:06:21 +0200

    solved

--- lib/presentation/main_screen/main_view.dart
+++ lib/presentation/main_screen/main_view.dart
@@ -13,83 +13,96 @@ class MainView extends StatefulWidget {
 class _MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: PreferredSize(
           preferredSize: const Size(double.infinity, 155),
           child: Stack(
               fit: StackFit.passthrough,
-              clipBehavior: Clip.none, children: [
-            Container(
-              height: 155,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: ColorManager.primary,
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  height: 155,
+                  decoration:  const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color.fromRGBO(97,206,255,0.45),
+                        Colors.white
+                      ],
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.bottomRight
+                    ),
+                  ),
                 ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                Container(
+                  height: 155,
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: ColorManager.primary,
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Hi Handwerker!',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeightManager.light,
-                          color: ColorManager.white,
-                        ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hi Handwerker!',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeightManager.light,
+                              color: ColorManager.white,
+                            ),
+                          ),
+                          Text(
+                            'Find Your Doctor',
+                            style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeightManager.bold,
+                              color: ColorManager.white,
+                            ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        'Find Your Doctor',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeightManager.bold,
-                          color: ColorManager.white,
-                        ),
+                      const CircleAvatar(
+                        radius: 30,
+                        foregroundImage: AssetImage(ImageAssets.personal),
                       ),
                     ],
                   ),
-                  const CircleAvatar(
-                    radius: 30,
-                    foregroundImage: AssetImage(ImageAssets.personal),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              left: 22,
-              right: 22,
-              bottom: -28,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: ColorManager.white,
-                  borderRadius: BorderRadius.circular(10),
                 ),
-                // width: 335,
-                height: 54,
-                child: Center(
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      fillColor: Colors.transparent,
-                      suffixIcon: Icon(Icons.close),
-                      prefixIcon: Icon(Icons.search_sharp),
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      hintText: 'Search....',
+                Positioned(
+                  left: 22,
+                  right: 22,
+                  bottom: -28,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: ColorManager.white,
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    onTap: () {},
+                    // width: 335,
+                    height: 54,
+                    child: Center(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          fillColor: Colors.transparent,
+                          suffixIcon: Icon(Icons.close),
+                          prefixIcon: Icon(Icons.search_sharp),
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          hintText: 'Search....',
+                        ),
+                        onTap: () {},
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-          ]),
+              ]),
         ),
         body: Container(
           decoration: const BoxDecoration(
